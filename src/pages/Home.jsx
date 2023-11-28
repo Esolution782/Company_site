5d91286d5b74ff910d1a43257dc0d40fac462524
ewaysolution <help.eway.solutions@gmail.com>
2023-11-29 02:18:06 +0530

    Image show problem solved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function index() {
   <>
     <nav className="navbar navbar-expand-xl fixed-top">
     <div className="container">
-      <Link className="navbar-brand" to="./index.html"><img className="w-100" src="../src/assets/img/icon/logo.svg" alt=""/></Link>
+      <Link className="navbar-brand" to="./index.html"><img className="w-100" src="../public/assets/img/icon/logo.svg" alt=""/></Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <i className="fas fa-bars"></i>
       </button>
@@ -49,21 +49,21 @@ export default function index() {
           </div>
         </div>
         <div className="intro-img">
-            <img src="../src/assets/img/header-img.png" alt=""/>
+            <img src="../public/assets/img/header-img.png" alt=""/>
         </div>
         <div className="scroll">
-          <Link to="#variations"><img src="../src/assets/img/icon/icon-scroll-down.svg" alt=""/><span className="text-muted">Scroll down</span></Link>
+          <Link to="#variations"><img src="../public/assets/img/icon/icon-scroll-down.svg" alt=""/><span className="text-muted">Scroll down</span></Link>
         </div>
       </div>
     
       <div className="clients text-center">
         <p className="text-muted text-uppercase pb-4">Some of our trusted clients</p>
         <div className="client-logo">
-          <div><img src="../src/assets/img/icon/go-sm-logo.svg" alt=""/></div>
-          <div><img src="../src/assets/img/icon/social-road-logo.svg" alt=""/></div>
-          <div><img src="../src/assets/img/icon/firmbee-logo.svg" alt=""/></div>
-          <div><img src="../src/assets/img/icon/create-paper.svg" alt=""/></div>
-          <div><img src="../src/assets/img/icon/point-design-logo.svg" alt=""/></div>
+          <div><img src="../public/assets/img/icon/go-sm-logo.svg" alt=""/></div>
+          <div><img src="../public/assets/img/icon/social-road-logo.svg" alt=""/></div>
+          <div><img src="../public/assets/img/icon/firmbee-logo.svg" alt=""/></div>
+          <div><img src="../public/assets/img/icon/create-paper.svg" alt=""/></div>
+          <div><img src="../public/assets/img/icon/point-design-logo.svg" alt=""/></div>
         </div>
       </div>
 
@@ -71,7 +71,7 @@ export default function index() {
       <div id="variations" className="variations">
         <div className="first-variation">
           <div className="img">
-            <img src="../src/assets/img/man.png" alt=""/>
+            <img src="../public/assets/img/man.png" alt=""/>
           </div>
           <div className="text">
             <h1 className="variations-h1">There are many variations of passages of Lorem Ipsum available</h1>
@@ -92,8 +92,8 @@ export default function index() {
             </div>
           </div>
           <div className="img">
-            <img src="../src/assets/img/woman.png" alt=""/>
-            <img className = "square" src="../src/assets/img/icon/square.png" alt=""/>
+            <img src="../public/assets/img/woman.png" alt=""/>
+            <img className = "square" src="../public/assets/img/icon/square.png" alt=""/>
           </div>
         </div>
       </div>
@@ -104,22 +104,22 @@ export default function index() {
         <p className="front-page">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         <div className="service-items">
           <div className="item">
-            <img src="../src/assets/img/icon/code-icon.svg" alt=""/>
+            <img src="../public/assets/img/icon/code-icon.svg" alt=""/>
             <span className="counter">7 453 664</span>
             <span>Kilometers <br/>of code written</span>
           </div>
           <div className="item">
-            <img src="../src/assets/img/icon/cofee-icon.svg" alt=""/>
+            <img src="../public/assets/img/icon/cofee-icon.svg" alt=""/>
             <span className="counter">43 657</span>
             <span>Coffees<br/>drunk</span>
           </div>
           <div className="item">
-            <img src="../src/assets/img/icon/satisfied-client-icon.svg" alt=""/>
+            <img src="../public/assets/img/icon/satisfied-client-icon.svg" alt=""/>
             <span className="counter">1 764</span>
             <span>Satisfied<br/>customers</span>
           </div>
           <div className="item">
-            <img src="../src/assets/img/icon/realized-projects-icon.svg" alt=""/>
+            <img src="../public/assets/img/icon/realized-projects-icon.svg" alt=""/>
             <span className="counter">36 874</span>
             <span>Realized<br/>projects</span>
           </div>
@@ -146,22 +146,22 @@ export default function index() {
         <p className="front-page">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         <div className="team-person">
           <div className="person">
-            <div className="photo"><img src="../src/assets/img/team-1.png" alt=""/></div>
+            <div className="photo"><img src="../public/assets/img/team-1.png" alt=""/></div>
             <span className="name">John Fox</span>
             <span>Marketing Manager</span>
           </div>
           <div className="person">
-            <div className="photo"><img src="../src/assets/img/team-2.png" alt=""/></div>
+            <div className="photo"><img src="../public/assets/img/team-2.png" alt=""/></div>
             <span className="name">Kate Rose</span>
             <span>SEO</span>
           </div>
           <div className="person">
-            <div className="photo"><img src="../src/assets/img/team-3.png" alt=""/></div>
+            <div className="photo"><img src="../public/assets/img/team-3.png" alt=""/></div>
             <span className="name">Anna Brown</span>
             <span>UI Expert</span>
           </div>
           <div className="person">
-            <div className="photo"><img src="../src/assets/img/team-4.png" alt=""/></div>
+            <div className="photo"><img src="../public/assets/img/team-4.png" alt=""/></div>
             <span className="name">Mike Doe</span>
             <span>Web Developer</span>
           </div>
