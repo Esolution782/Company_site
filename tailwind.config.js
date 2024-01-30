0d3b51fb84874a2dcc8b789472f96efb12ea7697
ewaysolution <help.eway.solutions@gmail.com>
2024-01-30 20:38:04 +0530

    initial disable

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        nunito: ["Nunito", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
-      },
-      animation: {
-        'spin-slow': 'spin 30s linear infinite',
-        
-       },
-    },
-   
+    extend: {},
   },
-  plugins: [ ],
-};
+  plugins: [],
+}
+
